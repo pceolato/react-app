229bbe84edd599953c87224cca38087df2440f25
pceolato <pedroceolato2002@gmail.com>
2022-04-22 22:11:32 -0300

    feat: atualização

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// uma função, que é um retorno HTML
+function Home() {
+  return (
+   <div>
+    <h1>Lista de Presença</h1>
+    <input type="text" placeholder="Digite o nome..." />
+    <button type="button">Adicionar</button>
+  </div>
+  )
+}
+
+export default Home
